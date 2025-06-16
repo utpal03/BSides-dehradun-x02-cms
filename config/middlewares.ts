4ce0e63883a90ee02550c603ab311c824d8f41b8
utpal <utpalkant1403@gmail.com>
2025-06-17 03:14:09 +0530

    integrate cloudinary for uploaf

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,20 @@
 export default [
   'strapi::logger',
-  'strapi::errors',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
